f384eb2d10f01fc8f12af5b520ec8dda981f64d9
Moses <moseswambui044@gmail.com>
2022-01-08 16:35:49 +0300

    adding s3 buckets

--- GlatexManagementSoftware/settings.py
+++ GlatexManagementSoftware/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'inventory',
     "accounts",
     'services',
+    'storages',
     
 ]
 
@@ -168,3 +169,29 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "moseswambui044@gmail.com"
 EMAIL_HOST_PASSWORD = "mjguofihaslzopfa"
+
+#S3 BUCKETS CONFIG
+
+AWS_ACCESS_KEY_ID = 'AKIAW2ECWHV4WILT75MW'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'glatexsignsafrica'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+'''
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<CORSRule>
+    <AllowedOrigin>*</AllowedOrigin>
+    <AllowedMethod>GET</AllowedMethod>
+    <AllowedMethod>POST</AllowedMethod>
+    <AllowedMethod>PUT</AllowedMethod>
+    <AllowedHeader>*</AllowedHeader>
+</CORSRule>
+</CORSConfiguration>
+'''
