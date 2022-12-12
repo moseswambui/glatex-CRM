40ed48a8fefecb7821a3b68f28aec5dc6acd95cc
moses044 <moseswambui044@gmail.com>
2022-12-12 20:45:53 +0300

    Other Changes

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,11 @@ from .models import Blog,Category
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
 
-<<<<<<< HEAD
 def Blog(request):
     return render(request, 'blog/blog.html')
 
 def Detail_blog(request):
     return render(request,'blog/single-detail-blog.html')
-=======
 def MyBlog(request, category_slug=None):
 
     categories = None
@@ -67,4 +65,3 @@ def search(request):
 
 
     return render(request, 'blog/blog.html', context)
->>>>>>> main
